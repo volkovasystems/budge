876facef5331dfdebe07bf397957e51a07f2ee7c
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-02 15:02:21 +0800

    Updated test.

--- budge-test.js
+++ budge-test.js
@@ -2,7 +2,7 @@
 const assert = require( "assert" );
 const budge = require( "./budge.js" );
 
-assert.deepEqual( budge( [ 1, 2, 3 ] ), [ 2, 3 ] );
+assert.deepEqual( budge( [ 1, 2, 3 ], 1 ), [ 2, 3 ] );
 
 assert.deepEqual( budge( [ 1, 2, 3 ], 2 ), [ 3 ] );
 
