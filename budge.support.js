12e11ebed56c0179381567539f4dcf89a4b32415
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-19 13:46:36 +0800

    Updated dependency.

--- budge.support.js
+++ budge.support.js
@@ -34,11 +34,11 @@
               			"file": "budge.js",
               			"module": "budge",
               			"author": "Richeve S. Bebedor",
+              			"eMail": "richeve.bebedor@gmail.com",
               			"contributors": [
               				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
               				"Vinse Vinalon <vinsevinalon@gmail.com>"
               			],
-              			"eMail": "richeve.bebedor@gmail.com",
               			"repository": "https://github.com/volkovasystems/budge.git",
               			"test": "budge-test.js",
               			"global": true
